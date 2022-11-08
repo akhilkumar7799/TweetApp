24a49e7667c2ff61f70960641d19d1800dc08203
akhilkumar7799 <57667250+akhilkumar7799@users.noreply.github.com>
2022-11-08 19:00:53 +0530

    Revert "Feature/akhil corejava"

--- src/com/learn/abstrac/TechPark.java
+++ src/com/learn/abstrac/TechPark.java
@@ -5,7 +5,7 @@ public class TechPark implements ItCompanies {
 	@Override
 	public void working() {
 		// TODO Auto-generated method stub
-		System.out.println("working tweetapp new");
+		System.out.println("working tweetapp");
 	}
 	public static void main(String[] args) {
 		
